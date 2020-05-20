ee645b68b3e5ffe71061067875f8a78d31dde854
jbshipman <jbshipman@icloud.com>
2020-05-20 10:54:01 -0500

    updated session store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,9 @@
-Rails.application.config.session_store :cookie_store, key: "_api_authentication", domain: "template_api.herokuapp.com"
+# Conditional statement to ensure that the session cookie is created by the correct source
+# while in production the cookie will be created by the hosted server
+# or the localhost if not production
+
+if Rails.env == "production"
+  Rails.application.config.session_store :cookie_store, key: "_api_authentication", domain: "template_api.herokuapp.com"
+else
+  Rails.application.config.session_store :cookie_store, key: "_api_authentication"
+end
