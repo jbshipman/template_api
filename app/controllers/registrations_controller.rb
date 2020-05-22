ff0620590fa038860252e488f2520b1f93483a77
jbshipman <jbshipman@icloud.com>
2020-05-22 12:54:03 -0500

    welcome email on registration

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,5 +1,6 @@
 class RegistrationsController < ApplicationController
   def create
+    # Take fontend inputs & save new record in User Model
     user = User.create!(
       username: params["user"]["username"],
       email: params["user"]["email"],
@@ -7,12 +8,17 @@ class RegistrationsController < ApplicationController
       password_confirmation: params["user"]["password_confirmation"],
     )
 
+    # Conditional to check user created
+    # put created user into session store
+    # render to frontend json object
+    # send welcome email to new user
     if user
       session[:user_id] = user.id
       render json: {
         status: :created,
         user: user,
       }
+      UserMailer.welcome_email(user).deliver_now
     else
       render json: { status: 500 }
     end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,6 @@
 class UserMailer < ApplicationMailer
+  def welcome_email(user)
+    @user = user
+    mail(to: @user.email, subject: "Welcome to [app name]")
+  end
 end
